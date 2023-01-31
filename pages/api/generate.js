e13a94df82e403d06bf83a0de38e6c5f4abd9342
alan-arguello <alan@jointrendi.com>
2023-01-30 22:29:50 -0600

    test

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -18,6 +18,13 @@ const generateAction = async (req, res) => {
     max_tokens: 250,
   });
 
+  const baseCompletion = await openai.createCompletion({
+    model: "text-davinci-003",
+    prompt: `${basePromptPrefix}${req.body.userInput}`,
+    temperature: 0.7,
+    max_tokens: 250,
+  });
+
   const basePromptOutput = baseCompletion.data.choices.pop();
 
   res.status(200).json({ output: basePromptOutput });
